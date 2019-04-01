57497753ff6a82110c19513fd9742073a6467178
Andreas Mueller <andreas.mueller@columbia.edu>
2019-04-01 09:26:10 -0400

    start website

--- dabl/__init__.py
+++
@@ -1,7 +0,0 @@
-from .preprocessing import EasyPreprocessor
-from .models import EasyClassifier
-from .plotting import plot_supervised
-from .search import GridSuccessiveHalving, RandomSuccessiveHalving
-
-__all__ = ['EasyPreprocessor', 'EasyClassifier', 'plot_supervised',
-           'GridSuccessiveHalving, RandomSuccessiveHalving']
--- dabl/tests/test_common.py
+++
@@ -1,8 +0,0 @@
-from sklearn.utils.estimator_checks import check_estimator
-from dabl.preprocessing import EasyPreprocessor
-import pytest
-
-
-@pytest.mark.skip(reason="haven't implemented numpy array type checks yet")
-def test_preprocessor():
-    return check_estimator(EasyPreprocessor)
