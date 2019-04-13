8553c96a752a7491cd344b8a8a817de37368a886
amueller <t3kcit+githubspam@gmail.com>
2019-04-13 00:15:13 +0000

    Pushing the docs to dev/ for branch: master, commit 6f7c2212a7044e4fc24ca1b5c2cb20b5daf7922a

--- dev/quick_start-1.py
+++ dev/quick_start-1.py
@@ -3,4 +3,5 @@ import dabl
 titanic = pd.read_csv(dabl.datasets.data_path("titanic.csv"))
 dabl.plot_supervised(titanic, 'survived')
 # Target looks like classification
+# Linear Discriminant Analysis training set score: 0.578
 import matplotlib.pyplot as plt; plt.show()
