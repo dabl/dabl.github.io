be5e178344e18e61a19575f8f17abce6b0d5dff0
Andreas C Mueller <andreasmuellerml@gmail.com>
2020-09-25 14:10:41 -0400

    fix version to 0.1.9

--- 0.1.10/quick_start-1.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import dabl
-titanic = pd.read_csv(dabl.datasets.data_path("titanic.csv"))
-dabl.plot(titanic, 'survived')
-# Target looks like classification
-# Linear Discriminant Analysis training set score: 0.578
-import matplotlib.pyplot as plt; plt.show()
