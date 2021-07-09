17f3c4ba7ffa01529eb7330777ba4d46400f1f8a
Andreas Mueller <amueller@microsoft.com>
2021-07-09 12:27:40 -0700

    add 0.2.2 release website

---
+++ 0.2.2/quick_start-1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import dabl
+titanic = pd.read_csv(dabl.datasets.data_path("titanic.csv"))
+dabl.plot(titanic, 'survived')
+# Target looks like classification
+# Linear Discriminant Analysis training set score: 0.578
+# [[<Figure size 1500x1500 with 30 Axes>, <Figure size 1600x400 with 4 Axes>, <Figure size 640x480 with 1 Axes>], None]
+
+import matplotlib.pyplot as plt; plt.show()
